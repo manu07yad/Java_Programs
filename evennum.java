140276cefcb67cd20e137c9420bedd56aecb9564
MANASVI YADAV <manasviyadav330@gmail.com>
2021-10-07 20:14:57 +0530

    Add files via upload

---
+++ evennum.java
@@ -0,0 +1,17 @@
+package evennum;
+
+public class evennum {
+
+	public static void main(String[] args) 
+	{
+		for(int i=100;i<=1000;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.print(i + " ");
+			}
+		}	
+
+	}
+
+}
