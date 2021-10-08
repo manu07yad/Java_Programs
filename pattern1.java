876b567d69ec95e71b32eb5db98129938ad354f6
MANASVI YADAV <manasviyadav330@gmail.com>
2021-10-08 22:23:43 +0530

    Add files via upload

---
+++ pattern1.java
@@ -0,0 +1,18 @@
+package pattern1;
+
+public class pattern1 {
+
+	public static void main(String[] args) 
+	{
+		for(int i=0;i<4;i++)
+		{
+			for(int j=0;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
